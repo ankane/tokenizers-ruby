ec82873035f627447d24a78b297c8966f2b3dc71
Andrew Kane <andrew@ankane.org>
2022-06-29 16:10:24 -0400

    Fixed error with installation - fixes #2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 (unreleased)
+
+- Fixed error with installation
+
 ## 0.1.0 (2022-03-19)
 
 - First release
--- ext/tokenizers/extconf.rb
+++ ext/tokenizers/extconf.rb
@@ -1,6 +1,10 @@
+require "pathname"
+
+dest = Pathname.new(__dir__).relative_path_from(Pathname.pwd).join("../../lib/tokenizers/ext.#{RbConfig::CONFIG["DLEXT"]}")
+
 File.write "Makefile", <<~EOS
   install:
-  \tcargo build --release
-  \tmv target/release/libtokenizers.#{RbConfig::CONFIG["SOEXT"]} lib/tokenizers/ext.#{RbConfig::CONFIG["DLEXT"]}
+  \tcargo build --release --target-dir target
+  \tmv target/release/libtokenizers.#{RbConfig::CONFIG["SOEXT"]} #{dest}
   clean:
 EOS
