bb3b303ef4bc06996a3561ca2d53b89ef0db927e
kojix2 <2xijok@gmail.com>
2022-09-09 12:59:19 +0900

    Fix (#3)

--- ext/tokenizers/extconf.rb
+++ ext/tokenizers/extconf.rb
@@ -5,6 +5,6 @@ dest = Pathname.new(__dir__).relative_path_from(Pathname.pwd).join("../../lib/to
 File.write "Makefile", <<~EOS
   install:
   \tcargo build --release --target-dir target
-  \tmv target/release/libtokenizers.#{RbConfig::CONFIG["SOEXT"]} #{dest}
+  \trsync target/release/libtokenizers.#{RbConfig::CONFIG["SOEXT"]} #{dest}
   clean:
 EOS
