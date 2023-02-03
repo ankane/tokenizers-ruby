50419f6e9c47cc8fac0a85c14f29b1830f91ca13
Peter Goldstein <peter.m.goldstein@gmail.com>
2023-02-02 20:18:30 -0500

    Add Decoders (#19)

--- ext/tokenizers/src/decoders.rs
+++ ext/tokenizers/src/decoders.rs
@@ -7,6 +7,10 @@ use magnus::{
 };
 use serde::{Deserialize, Serialize};
 use tk::decoders::bpe::BPEDecoder;
+use tk::decoders::byte_level::ByteLevel;
+use tk::decoders::ctc::CTC;
+use tk::decoders::metaspace::Metaspace;
+use tk::decoders::wordpiece::WordPiece;
 use tk::decoders::DecoderWrapper;
 use tk::Decoder;
 
@@ -27,8 +31,40 @@ impl Decoder for RbDecoder {
 pub struct RbBPEDecoder {}
 
 impl RbBPEDecoder {
+    pub fn new(suffix: String) -> RbDecoder {
+        BPEDecoder::new(suffix).into()
+    }
+}
+
+pub struct RbByteLevelDecoder {}
+
+impl RbByteLevelDecoder {
     pub fn new() -> RbDecoder {
-        BPEDecoder::default().into()
+        ByteLevel::default().into()
+    }
+}
+
+pub struct RbCTC {}
+
+impl RbCTC {
+    pub fn new(pad_token: String, word_delimiter_token: String, cleanup: bool) -> RbDecoder {
+        CTC::new(pad_token, word_delimiter_token, cleanup).into()
+    }
+}
+
+pub struct RbMetaspaceDecoder {}
+
+impl RbMetaspaceDecoder {
+    pub fn new(replacement: char, add_prefix_space: bool) -> RbDecoder {
+        Metaspace::new(replacement, add_prefix_space).into()
+    }
+}
+
+pub struct RbWordPieceDecoder {}
+
+impl RbWordPieceDecoder {
+    pub fn new(prefix: String, cleanup: bool) -> RbDecoder {
+        WordPiece::new(prefix, cleanup).into()
     }
 }
 
@@ -89,6 +125,26 @@ unsafe impl TypedData for RbDecoder {
                     class.undef_alloc_func();
                     class
                 }),
+                DecoderWrapper::ByteLevel(_) => *memoize!(RClass: {
+                    let class: RClass = module().const_get("ByteLevelDecoder").unwrap();
+                    class.undef_alloc_func();
+                    class
+                }),
+                DecoderWrapper::CTC(_) => *memoize!(RClass: {
+                    let class: RClass = module().const_get("CTC").unwrap();
+                    class.undef_alloc_func();
+                    class
+                }),
+                DecoderWrapper::Metaspace(_) => *memoize!(RClass: {
+                    let class: RClass = module().const_get("MetaspaceDecoder").unwrap();
+                    class.undef_alloc_func();
+                    class
+                }),
+                DecoderWrapper::WordPiece(_) => *memoize!(RClass: {
+                    let class: RClass = module().const_get("WordPieceDecoder").unwrap();
+                    class.undef_alloc_func();
+                    class
+                }),
                 _ => todo!(),
             },
         }
@@ -99,7 +155,19 @@ pub fn decoders(module: &RModule) -> RbResult<()> {
     let decoder = module.define_class("Decoder", Default::default())?;
 
     let class = module.define_class("BPEDecoder", decoder)?;
-    class.define_singleton_method("new", function!(RbBPEDecoder::new, 0))?;
+    class.define_singleton_method("_new", function!(RbBPEDecoder::new, 1))?;
+
+    let class = module.define_class("ByteLevelDecoder", decoder)?;
+    class.define_singleton_method("new", function!(RbByteLevelDecoder::new, 0))?;
+
+    let class = module.define_class("CTC", decoder)?;
+    class.define_singleton_method("_new", function!(RbCTC::new, 3))?;
+
+    let class = module.define_class("MetaspaceDecoder", decoder)?;
+    class.define_singleton_method("_new", function!(RbMetaspaceDecoder::new, 2))?;
+
+    let class = module.define_class("WordPieceDecoder", decoder)?;
+    class.define_singleton_method("_new", function!(RbWordPieceDecoder::new, 2))?;
 
     Ok(())
 }
--- test/decoder_test.rb
+++ test/decoder_test.rb
@@ -5,5 +5,61 @@ class DecoderTest < Minitest::Test
     decoder = Tokenizers::BPEDecoder.new
     assert_instance_of Tokenizers::BPEDecoder, decoder
     assert_kind_of Tokenizers::Decoder, decoder
+
+    decoder = Tokenizers::BPEDecoder.new(suffix: "</end>")
+    assert_instance_of Tokenizers::BPEDecoder, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+  end
+
+  def test_byte_level
+    decoder = Tokenizers::ByteLevelDecoder.new
+    assert_instance_of Tokenizers::ByteLevelDecoder, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+  end
+
+  def test_ctc
+    decoder = Tokenizers::CTC.new
+    assert_instance_of Tokenizers::CTC, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+
+    decoder = Tokenizers::CTC.new(pad_token: "<mypad>")
+    assert_instance_of Tokenizers::CTC, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+
+    decoder = Tokenizers::CTC.new(word_delimiter_token: "+")
+    assert_instance_of Tokenizers::CTC, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+
+    decoder = Tokenizers::CTC.new(cleanup: false)
+    assert_instance_of Tokenizers::CTC, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+  end
+
+  def test_metaspace
+    decoder = Tokenizers::MetaspaceDecoder.new
+    assert_instance_of Tokenizers::MetaspaceDecoder, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+
+    decoder = Tokenizers::MetaspaceDecoder.new(replacement: "+")
+    assert_instance_of Tokenizers::MetaspaceDecoder, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+
+    decoder = Tokenizers::MetaspaceDecoder.new(add_prefix_space: false)
+    assert_instance_of Tokenizers::MetaspaceDecoder, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+  end
+
+  def test_word_piece
+    decoder = Tokenizers::WordPieceDecoder.new
+    assert_instance_of Tokenizers::WordPieceDecoder, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+
+    decoder = Tokenizers::WordPieceDecoder.new(prefix: "+")
+    assert_instance_of Tokenizers::WordPieceDecoder, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
+
+    decoder = Tokenizers::WordPieceDecoder.new(cleanup: false)
+    assert_instance_of Tokenizers::WordPieceDecoder, decoder
+    assert_kind_of Tokenizers::Decoder, decoder
   end
 end
