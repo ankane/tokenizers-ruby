80713bce6c3e61e1f6ededab189ce7bcd1ab003d
Andrew Kane <andrew@ankane.org>
2023-02-02 20:32:08 -0800

    Use **kwargs [skip ci]

--- lib/tokenizers/models/bpe.rb
+++ lib/tokenizers/models/bpe.rb
@@ -1,15 +1,7 @@
 module Tokenizers
   module Models
     class BPE
-      def self.new(vocab: nil, merges: nil, cache_capacity: nil, dropout: nil, unk_token: nil,
-                   continuing_subword_prefix: nil, end_of_word_suffix: nil, fuse_unk: nil)
-        kwargs = {}
-        kwargs[:cache_capacity] = cache_capacity if cache_capacity
-        kwargs[:dropout] = dropout if dropout
-        kwargs[:unk_token] = unk_token if unk_token
-        kwargs[:continuing_subword_prefix] = continuing_subword_prefix if continuing_subword_prefix
-        kwargs[:end_of_word_suffix] = end_of_word_suffix if end_of_word_suffix
-        kwargs[:fuse_unk] = fuse_unk if fuse_unk
+      def self.new(vocab: nil, merges: nil, **kwargs)
         _new(vocab, merges, kwargs)
       end
     end
--- lib/tokenizers/models/word_piece.rb
+++ lib/tokenizers/models/word_piece.rb
@@ -1,11 +1,7 @@
 module Tokenizers
   module Models
     class WordPiece
-      def self.new(vocab: nil, unk_token: nil, max_input_chars_per_word: nil, continuing_subword_prefix: nil)
-        kwargs = {}
-        kwargs[:unk_token] = unk_token if unk_token
-        kwargs[:max_input_chars_per_word] = max_input_chars_per_word if max_input_chars_per_word
-        kwargs[:continuing_subword_prefix] = continuing_subword_prefix if continuing_subword_prefix
+      def self.new(vocab: nil, **kwargs)
         _new(vocab, kwargs)
       end
     end
