557a1efaba06b2ea6299f7dc51307d2f4b2aeab8
Andrew Kane <andrew@ankane.org>
2026-04-27 14:51:35 -0700

    Moved unwrap [skip ci]

--- ext/tokenizers/src/error.rs
+++ ext/tokenizers/src/error.rs
@@ -9,20 +9,20 @@ pub struct RbError {}
 impl RbError {
     // convert to Error instead of Self
     pub fn from(e: Box<dyn std::error::Error + Send + Sync>) -> Error {
-        Error::new(error(), e.to_string())
+        Error::new(error(&Ruby::get().unwrap()), e.to_string())
     }
 
     pub fn new_err<T>(s: T) -> Error
     where
         T: Into<Cow<'static, str>>,
     {
-        Error::new(error(), s)
+        Error::new(error(&Ruby::get().unwrap()), s)
     }
 }
 
 static ERROR: Lazy<ExceptionClass> =
     Lazy::new(|ruby| ruby.get_inner(&TOKENIZERS).const_get("Error").unwrap());
 
-fn error() -> ExceptionClass {
-    Ruby::get().unwrap().get_inner(&ERROR)
+fn error(ruby: &Ruby) -> ExceptionClass {
+    ruby.get_inner(&ERROR)
 }
